91c807889cc7e7fc2a021c45da5714a69163ecf8
akramreiter <a.kramreiter@gmx.at>
2021-10-12 09:01:34 +0200

    [DEV] Added CSV card reading

--- src/main/java/org/kramreiter/mtcg/Launch.java
+++ src/main/java/org/kramreiter/mtcg/Launch.java
@@ -4,12 +4,12 @@ import org.kramreiter.mtcg.card.*;
 
 public class Launch {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
-        Card knight = new CardMonster("Basic Boring Human Knight", 50, 0, false, 3, null, 0);
+        Card knight = new CardMonster("Basic Boring Human Knight", 50, 0, 3, null, 0);
         System.out.println(knight);
 
-        Card fireball = new CardSpell("Fireball", 40, CardType.Fire, true, Rarity.Rare, null);
+        CardFactory.getCard("hi", Rarity.Legendary);
+
+        Card fireball = new CardSpell("Fireball", 40, CardType.Fire, Rarity.Rare, null);
         System.out.println(fireball);
         System.out.println("effectiveness of " + fireball.getName() + " against " + knight.getName() + ": " + RuleManager.computeWeaknessMultiplier(fireball.getCardType(), knight.getCardType()));
     }
--- src/main/java/org/kramreiter/mtcg/card/CardFactory.java
+++ src/main/java/org/kramreiter/mtcg/card/CardFactory.java
@@ -1,12 +1,53 @@
 package org.kramreiter.mtcg.card;
 
+import com.opencsv.CSVReaderHeaderAware;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
 public class CardFactory {
+    private static final String FILE_COMMON = "src/main/resources/cards_common.csv";
+    private static final String FILE_RARE = "src/main/resources/cards_rare.csv";
+    private static final String FILE_EPIC = "src/main/resources/cards_epic.csv";
+    private static final String FILE_LEGENDARY = "src/main/resources/cards_legendary.csv";
 
     public static Card getCard(String cardId) {
         return null;
     }
 
     public static Card getCard(String cardId, Rarity rarity) {
+        String filename = fileFromRarity(rarity);
+        try {
+            String[] card;
+            CSVReaderHeaderAware reader = new CSVReaderHeaderAware(new FileReader(filename));
+            while ((card = reader.readNext()) != null) {
+                if (card[0].equals(cardId)) {
+                    if (card[3].equals("1")) {
+                        return new CardSpell(
+                                card[1],
+                                Integer.parseInt(card[4]),
+                                Integer.parseInt(card[2]),
+                                Integer.parseInt(card[5]),
+                                card[7]
+                        );
+                    } else {
+                        return new CardMonster(
+                                card[1],
+                                Integer.parseInt(card[4]),
+                                Integer.parseInt(card[2]),
+                                Integer.parseInt(card[5]),
+                                card[7],
+                                Integer.parseInt(card[6])
+                        );
+                    }
+                }
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -15,6 +56,11 @@ public class CardFactory {
     }
 
     private static String fileFromRarity(Rarity rarity) {
-        return "";
+        return switch (rarity) {
+            case Common -> FILE_COMMON;
+            case Rare -> FILE_RARE;
+            case Epic -> FILE_EPIC;
+            case Legendary -> FILE_LEGENDARY;
+        };
     }
 }
