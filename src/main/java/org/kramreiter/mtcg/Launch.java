015c04a39ed9237746981419c892124ea3963b71
akramreiter <a.kramreiter@gmx.at>
2021-10-13 11:28:56 +0200

    [DEV] changed Launch.java

--- src/main/java/org/kramreiter/mtcg/Launch.java
+++ src/main/java/org/kramreiter/mtcg/Launch.java
@@ -4,13 +4,17 @@ import org.kramreiter.mtcg.card.*;
 
 public class Launch {
     public static void main(String[] args) {
-        Card knight = new CardMonster("Basic Boring Human Knight", 50, 0, 3, null, 0);
-        System.out.println(knight);
-
-        CardFactory.getCard("hi", Rarity.Legendary);
-
-        Card fireball = new CardSpell("Fireball", 40, CardType.Fire, Rarity.Rare, null);
-        System.out.println(fireball);
-        System.out.println("effectiveness of " + fireball.getName() + " against " + knight.getName() + ": " + RuleManager.computeWeaknessMultiplier(fireball.getCardType(), knight.getCardType()));
+        Deck player1 = new Deck(new String[] {
+                "3001",
+                "3002",
+                "3003",
+                "3004"
+        }, "PLAYER1");
+        Deck player2 = new Deck(new String[] {
+                "3001",
+                "3002",
+                "3003",
+                "3004"
+        }, "PLAYER1");
     }
 }
